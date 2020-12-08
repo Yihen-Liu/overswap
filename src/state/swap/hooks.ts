a091000381ef5778322e83d3d773095d2fdd0b7a
Yihen <liuyihen@gmail.com>
2020-12-08 18:57:21 +0800

    deploy initPair contract

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -89,9 +89,9 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 }
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
-  '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', // v2 factory
+  '0x807F94047d7cA2E4544F1f8Ae47FB3274715e679', // v2 factory
   '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D' // v2 router 02
+  '0xD8f055de1a06629DE3372c73382a91201D18ac75' // v2 router 02
 ]
 
 /**
